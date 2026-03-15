bae686a8cb2f4c6ad038c45d42c99e6044547bbe
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-03-15 22:39:17 +0200

    1. now server is listening some port and working, state: ready to use 2. added eslinter

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("hello");
+const express = require("express");
+
+const app = express();
+require('dotenv').config();
+
+app.listen(process.env.PORT,()=> {
+    console.log("server is working");
+})
