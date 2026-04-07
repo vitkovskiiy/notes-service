996b3b122fa810799b9e58b8ed8b5a550ab3a1d2
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-04-07 11:52:43 +0300

    fix func import

--- src/routes/notes.router.js
+++ src/routes/notes.router.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const generateHtml = require("../../server")
 
 const generateHtml = (title, body) => `
 <!DOCTYPE html>
