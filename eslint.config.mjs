0305ccbc20de394497c50fbb6f22f856a2c91d8d
Ivan Vitkovskiy <cloudfly97@icloud.com>
2026-04-28 21:24:29 +0300

    dads

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,28 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    
+    files: ["**/*.js", "**/*.mjs", "**/*.cjs"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "no-undef": "error",
+      "no-console": "off",
+    },
+  },
+  
+  {
+    ignores: ["coverage/**"],
+  },
+];
